da05d2c23c4a1924df4ba5d3376cc434863a6b8f
Brad <brad.anderson58@gmail.com>
2019-11-23 12:53:45 -0500

    dynamically loading player sprites

--- frontend/src/scenes/game.scene.ts
+++ frontend/src/scenes/game.scene.ts
@@ -1,4 +1,5 @@
 import {Character} from "../intefaces/character.interface";
+import * as chara3 from "../assets/chara3.png";
 
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
@@ -21,23 +22,36 @@ export class GameScene extends Phaser.Scene {
 
   public preload(): void {
     this.loading = this.add.text(20, 20, 'Loading Game...');
-    //this.load.spritesheet('player', this.character.spriteSheet, this.character.walkAnimation);
+    import(`../assets/${this.character.spriteSheet}.png`)
+      .then(src => {
+        this.load.spritesheet('player', src, {frameWidth: 78, frameHeight: 108});
+      });
 
+    console.log(chara3);
+    this.load.spritesheet('test', chara3, {frameWidth: 78, frameHeight: 108});
+
+    this.loading.destroy();
   }
 
   public create() {
-    /*const sprite = this.add.sprite(0, 0, this.character.spriteSheet)
-      .setDisplaySize(0, 0)
-      .setName('display-sprite');
+    console.log('nothing');
+    this.createSprite('test');
+    this.createSprite('player', 400, 300);
+  }
+
+  public createSprite(name: string, x: number = 200, y: number = 200) {
+    const sprite = this.add.sprite(x, y, name)
+      //.setDisplaySize(0, 0)
+      .setName(`${name}-sprite`);
 
     this.anims.create({
-      key: 'display-anim',
-      frames: this.anims.generateFrameNumbers(this.character.spriteSheet, this.character.walkAnimation),
+      key: `${name}-anim`,
+      frames: this.anims.generateFrameNumbers(name, this.character.walkAnimation),
       frameRate: 6,
       repeat: -1
     });
 
-    sprite.anims.load('display-anim');
-    sprite.anims.play('display-anim');*/
+    sprite.anims.load(`${name}-anim`);
+    sprite.anims.play(`${name}-anim`);
   }
 }
